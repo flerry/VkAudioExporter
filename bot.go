cbe00f93b2731f5dc722a9d8f31a704ee723d1cb
Stanislav <nuryelse@gmail.com>
2017-05-07 10:07:01 +0300

    Add files via upload

---
+++ bot.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+	"time"
+	"vkAudioBot/vk"
+	"github.com/emirpasic/gods/sets/hashset"
+	"strconv"
+)
+
+const (
+	START_MSG = "Привет, человек :) Ты наверняка тот, кого задели жадные правообладатели vk." +
+		" Позволь, я помогу тебе скачать твои аудио в замечательное место - Telegram..." +
+		" Знаешь, я уверен, что сюда эти ужасные люди не дойдут. Ведь Дуров не позволит же, правда?\n" +
+		"Кстати, чтобы экспортировать все свои треки, напиши /getAll твой_vk_id. " +
+		"Например: /getAll 151665536\n\n[ВНИМАНИЕ]: ВАШИ АУДИОЗАПИСИ ДОЛЖНЫ БЫТЬ ОТКРЫТЫМИ!\n\n" +
+		"ID - это набор чисел, который можно узнать так: http://imgur.com/yO2JeNu"
+)
+
+func init() {
+	formatDate := time.Now().Local().Format("2006-01-02")
+	formatTime := time.Now().Local().Format("15:00:00")
+
+	f, err := os.Create("log" + formatDate + ".txt")
+
+	if err != nil {
+		log.Fatalf("Error opening file: %v", err)
+	}
+
+	f.Write([]byte(formatTime))
+
+	log.SetOutput(f)
+}
+
+func main() {
+	bannedUsers := hashset.New()
+
+	bot, err := tgbotapi.NewBotAPI("300531701:AAE93B5-9jUozeru6w-oLGTGW1raIc75hnE") //314564211:AAH59sKgMcht-F_sVevp3jGXLo9j2VELRqg CASLBOT 300531701:AAE93B5-9jUozeru6w-oLGTGW1raIc75hnE REALBOT
+	if err != nil {
+		panic(err)
+	}
+
+	bot.Debug = false
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s %d] %s", update.Message.From.UserName, update.Message.From.ID, update.Message.Text)
+
+		switch {
+		case bannedUsers.Contains(update.Message.Chat.ID):
+			{
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Извини, на твой аккаунт временно наложены ограничения."+
+					" Если есть какие-то вопросы, пиши сюда - @Flerry")
+				msg.ReplyToMessageID = update.Message.MessageID
+
+				bot.Send(msg)
+			}
+		case update.Message.Text == "/start":
+			{
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, START_MSG)
+				msg.ReplyToMessageID = update.Message.MessageID
+
+				bot.Send(msg)
+			}
+
+		case strings.Contains(update.Message.Text, "/getAll"):
+			{
+				re := regexp.MustCompile("[0-9]+")
+				if re.FindAllString(update.Message.Text, -1) == nil {
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Извините, но скорее всего Вы не ввели свой ID, либо ввели буквенный ID, что не прокатит ^_^.  Мне кажется, стоит "+
+						"ввести свой числовой ID... \nНайти его можно так: http://imgur.com/yO2JeNu")
+					bot.Send(msg)
+				} else {
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Я уже ушел за твоими аудио... Если я долго не появляюсь - все в порядке, просто у меня очередь :)")
+					bot.Send(msg)
+
+					go vk.GetAudio(false, strings.Replace(update.Message.Text, "/getAll ", "", -1), update.Message, bot)
+				}
+			}
+
+		case strings.Contains(update.Message.Text, "/getFromEnd"):
+			{
+				re := regexp.MustCompile("[0-9]+")
+				if re.FindAllString(update.Message.Text, -1) == nil {
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Извините, но скорее всего Вы не ввели свой ID, либо ввели буквенный ID, что не прокатит ^_^.  Мне кажется, стоит "+
+						"ввести свой числовой ID... \nНайти его можно так: http://imgur.com/yO2JeNu")
+					bot.Send(msg)
+				} else {
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Я уже ушел за твоими аудио... Если я долго не появляюсь - все в порядке, просто у меня очередь :)")
+					bot.Send(msg)
+
+					go vk.GetAudio(true, strings.Replace(update.Message.Text, "/getFromEnd ", "", -1), update.Message, bot)
+				}
+			}
+
+		case update.Message.Text == "/help":
+			{
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Ну, значит, пока я умею это:\n"+
+					"/getAll - скачать все аудио, начиная с самых новых\n"+
+					"Пример: /getAll 123252561\n\n"+
+					"/getFromEnd - скачать все аудио, начиная с самых старых\n"+
+					"Пример: - /getFromEnd 123252561")
+				msg.ReplyToMessageID = update.Message.MessageID
+				bot.Send(msg)
+			}
+		case strings.Contains(update.Message.Text, "/ban"):
+			{
+				banId := strings.Replace(update.Message.Text, "/ban ", "", -1)
+
+				bannedUsers.Add(strconv.ParseInt(banId, 10, 64))
+
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Забанил :)")
+				msg.ReplyToMessageID = update.Message.MessageID
+				bot.Send(msg)
+			}
+		case update.Message.Text == "/clearBanList" && update.Message.Chat.ID == 203110206:
+			{
+				bannedUsers.Clear()
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Отчистил :)")
+				msg.ReplyToMessageID = update.Message.MessageID
+				bot.Send(msg)
+			}
+		default:
+			{
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Хм, даже не знаю, что тебе ответить."+
+					" Если хочешь, чтобы я научился отвечать на такие команды, напиши моему создателю - @Flerry. Можно написать еще отзыв: https://storebot.me/bot/vksoundbot")
+				msg.ReplyToMessageID = update.Message.MessageID
+
+				bot.Send(msg)
+			}
+		}
+	}
+}
---
+++ vk/vkAudio.go
@@ -0,0 +1,165 @@
+package vk
+
+import (
+	"io/ioutil"
+	"strings"
+	"net/http"
+	"encoding/json"
+	"strconv"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+	"github.com/cydev/zero"
+	"net/url"
+)
+
+const (
+	AUDIO_GET = "https://api.vk.com/method/audio.get?access_token=" +
+		"b00e0925535b7f5beaf7fe6f01f3fc545d2144820b2c639a62e04c376ca55ebe408737b58962ee037b6f7&owner_id="
+	AUDIO_COUNT = "https://api.vk.com/method/audio.getCount?" +
+		"access_token=71b1ca0aca0fb70b4c485efa182d75854c1c3f9f27826253b1ec377a487a2afbfa42bc4d4d98bc22b362c&owner_id="
+	USER_AGENT   = "VKAndroidApp/4.9-1118 (Android 5.1; SDK 22; armeabi-v7a; UMI IRON; ru)"
+	PROXY        = "http://46.8.29.52:65233" //"http://46.8.29.52:65233" "http://77.73.65.155:8080"
+)
+
+type AudioCount struct {
+	CountAudio int `json:"response"`
+}
+
+type AudioList struct {
+	Response []struct {
+		Artist string `json:"artist"`
+		Title  string `json:"title"`
+		URL    string `json:"url,omitempty"`
+	} `json:"response"`
+}
+
+func downloadFile(URL string, name string) (file tgbotapi.FileReader, response *http.Response) {
+	resp, err := http.Get(URL)
+	if err != nil {
+		log.Printf(err.Error())
+	}
+
+	contentLength, _ := strconv.Atoi(resp.Header.Get("Content-Length"))
+
+	file = tgbotapi.FileReader{Name: name, Reader: resp.Body, Size: int64(contentLength)}
+
+	return file, resp
+}
+
+func exportFileToChat(URL string, name string, message *tgbotapi.Message, bot *tgbotapi.BotAPI) {
+	file, response := downloadFile(URL, name)
+
+	defer response.Body.Close()
+
+	audioConfig := tgbotapi.NewAudioUpload(message.Chat.ID, file)
+
+	bot.Send(audioConfig)
+}
+
+func GetAudio(fromEnd bool, id string, message *tgbotapi.Message, bot *tgbotapi.BotAPI) {
+	audioList := AudioList{}
+
+	proxyUrl, _ := url.Parse(PROXY)
+	clientWithProxy := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}}
+
+	request, _ := http.NewRequest("GET", AUDIO_GET+id, nil)
+	request.Header.Set("User-Agent", USER_AGENT)
+
+	response, err := clientWithProxy.Do(request)
+
+	if err != nil {
+		log.Printf(err.Error())
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		log.Printf(err.Error())
+	}
+
+	newJSON := strings.Replace(string(body), strconv.Itoa(getAudioCount(id))+",", "", 1)
+
+	json.Unmarshal([]byte(newJSON), &audioList)
+
+	if zero.IsZero(audioList) {
+		msg := tgbotapi.NewMessage(message.Chat.ID, "Мне кажется, у тебя закрыты аудио-записи или их просто нет... Это все массонннннны!!!")
+		msg.ReplyToMessageID = message.MessageID
+
+		bot.Send(msg)
+	}
+
+	if fromEnd {
+		if len(audioList.Response) != 0 {
+
+			for elem := len(audioList.Response) - 1; elem != 0; elem-- {
+				audioEntity := audioList.Response[elem]
+
+				name := audioEntity.Title
+				URL := audioEntity.URL
+
+				if URL != "" {
+					exportFileToChat(URL, name, message, bot)
+				}
+			}
+		} else {
+			msg := tgbotapi.NewMessage(message.Chat.ID, "Слушай, а ты не забыл пробел между /getFromEnd и твоим id??? Или лишний может поставил... Все в этой жизни важно!")
+			msg.ReplyToMessageID = message.MessageID
+
+			bot.Send(msg)
+			return
+		}
+	} else {
+		if len(audioList.Response) != 0 {
+			for elem := range audioList.Response {
+				audioEntity := audioList.Response[elem]
+
+				name := audioEntity.Title
+				URL := audioEntity.URL
+
+				if URL != "" {
+					exportFileToChat(URL, name, message, bot)
+				}
+			}
+		} else {
+			msg := tgbotapi.NewMessage(message.Chat.ID, "Слушай, а ты не забыл пробел между /getAll и твоим id??? Или лишний может поставил... Все в этой жизни важно!")
+			msg.ReplyToMessageID = message.MessageID
+
+			bot.Send(msg)
+
+			return
+		}
+	}
+
+	msg := tgbotapi.NewMessage(message.Chat.ID, "Я закончил... Думаю, тебе понравится результат :) \nИ если это так... То мой создатель был бы рад, если бы ты оценил меня здесь: https://storebot.me/bot/vksoundbot")
+	msg.ReplyToMessageID = message.MessageID
+
+	bot.Send(msg)
+}
+
+func getAudioCount(id string) (count int) {
+	audioCount := AudioCount{}
+	client := &http.Client{}
+
+	request, _ := http.NewRequest("GET", AUDIO_COUNT+id, nil)
+	request.Header.Set("User-Agent", USER_AGENT)
+
+	response, err := client.Do(request)
+
+	if err != nil {
+		log.Printf(err.Error())
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		log.Printf(err.Error())
+	}
+
+	json.Unmarshal(body, &audioCount)
+
+	return audioCount.CountAudio
+}
